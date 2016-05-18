4a5a9a69fbfa0f37e1dcbd290b71b23b2eea7653
Ryan Krauss <ryanwkrauss@gmail.com>
2016-05-18 15:03:19 -0500

    first capture attempt

--- camera_test.py
+++ camera_test.py
@@ -1,8 +1,13 @@
 from picamera import PiCamera
 from time import sleep
+import time
 
 camera = PiCamera()
 
-camera.start_preview()
-sleep(10)
-camera.stop_preview()
+#camera.start_preview()
+#sleep(10)
+#camera.stop_preview()
+t1 = time.time()
+camera.capture('/home/pi/Desktop/image.jpg')
+t2 = time.time()
+print('dt = ' + str(t2-t1))
