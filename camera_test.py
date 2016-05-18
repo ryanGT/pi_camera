057a542e8e2813ce0905565782bfc1f4e516b480
Ryan Krauss <ryanwkrauss@gmail.com>
2016-05-18 15:05:47 -0500

    pi camera capture attempt3

--- camera_test.py
+++ camera_test.py
@@ -5,10 +5,10 @@ import time
 camera = PiCamera()
 camera.rotation = 180
 
-#camera.start_preview()
+camera.start_preview()
 #sleep(10)
-#camera.stop_preview()
 t1 = time.time()
 camera.capture('/home/pi/Desktop/image.jpg')
 t2 = time.time()
 print('dt = ' + str(t2-t1))
+camera.stop_preview()
